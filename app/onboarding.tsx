2b0d96c0f9ae0d3070609daddf2810b8824f89bc
Mo <78183098+martinomeiza@users.noreply.github.com>
2024-07-01 15:25:30 -0230

    small tweaks to structure

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import { Image, View } from 'react-native'
-import { Text } from '~/components/ui/text'
-import { SafeAreaView } from '~/components/SafeAreaView'
-import { Button } from '~/components/ui/button'
+import React from "react";
+import { Image, View } from "react-native";
+import { Text } from "~/components/ui/text";
+import { SafeAreaView } from "~/components/SafeAreaView";
+import { Button } from "~/components/ui/button";
 
 const OnboardingScreen = () => {
   return (
-    <SafeAreaView className='items-center'>
-      <Image 
-        source={require("../assets/images/screens/onboarding/slide-1-image.png")}
-        className='mt-5'
-      />
-      <Text
-        className="text-center text-lg mt-2"
-      >
-        Quiz Dash App helps you conquer your studies with ease.
-      </Text>
-      <Text className="text-center text-sm text-primary/50">
-        By clicking Get started, you agree to our
-        {" "}<Text className='text-sm'>Terms of Service</Text>{" "}
-        and
-        {" "}<Text className='text-sm'>Privacy Policy</Text>{" "}
-      </Text>
-      <View className='flex flex-col items-center gap-y-5 mt-10 w-full'>
-        <Button className="w-[90%]">Get started</Button>
-        <Button className="w-[90%]" variant='secondary'>Login</Button>
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <View className="w-[90%]">
+        <Image
+          source={require("../assets/images/screens/onboarding/slide-1-image.png")}
+          className="mt-5 mx-auto"
+        />
+        <Text className="text-center text-lg mt-2">
+          Quiz Dash App helps you conquer your studies with ease.
+        </Text>
+        <Text className="text-center text-sm text-primary/50 my-2">
+          By clicking Get started, you agree to our{" "}
+          <Text className="text-sm">Terms of Service</Text> and{" "}
+          <Text className="text-sm">Privacy Policy</Text>{" "}
+        </Text>
+        <View className="md:max-w-[30em] flex flex-wrap flex-row justify-between items-center gap-y-5 mt-10 mx-auto w-full md:flex-row">
+          <Button className="w-full md:w-[49%] capitalize">Get started</Button>
+          <Button className="w-full md:w-[49%]" variant="secondary">
+            Login
+          </Button>
+        </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default OnboardingScreen
+export default OnboardingScreen;
