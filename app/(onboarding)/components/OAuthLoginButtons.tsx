723555179c5b9be0535bfdf039fbca24851f0458
Emmanuel Otobo <eotobo11@gmail.com>
2024-07-31 09:27:16 -0230

    Installed Google OAuth package

--- app/(onboarding)/components/OAuthLoginButtons.tsx
+++ app/(onboarding)/components/OAuthLoginButtons.tsx
@@ -1,13 +1,18 @@
-import { View, Image } from 'react-native'
+import { View, Image, Platform, Pressable } from 'react-native'
 import React from 'react'
 import { Separator } from '~/components/ui/separator'
 import { useColorScheme } from '~/lib/useColorScheme'
 import { Text } from '~/components/ui/text'
 import { useTranslation } from 'react-i18next'
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const OAuthLoginButtons = () => {
   const { isDarkColorScheme } = useColorScheme()
   const { t } = useTranslation("onboarding", { keyPrefix: "AuthScreens" });
+
+  const handleGoogleAuth = () => {
+    GoogleSignin.configure();
+  }
 
   return (
     <View className="gap-y-8">
@@ -17,12 +22,17 @@ const OAuthLoginButtons = () => {
         <Separator />
       </View>
       <View className="flex-row justify-center gap-x-8">
-        <Image source={require("~/assets/images/screens/google.png")} />
+        <Pressable onPress={handleGoogleAuth}>
+          <Image source={require("~/assets/images/screens/google.png")} />
+        </Pressable>
         <Image source={require("~/assets/images/screens/facebook.png")} />
         {
-          isDarkColorScheme ?
-          <Image source={require("~/assets/images/screens/apple-light.png")} /> :
-          <Image source={require("~/assets/images/screens/apple.png")} />
+          Platform.OS === 'ios' ?
+            (
+              isDarkColorScheme ?
+              <Image source={require("~/assets/images/screens/apple-light.png")} /> :
+              <Image source={require("~/assets/images/screens/apple.png")} />
+            ) : null
         }
       </View>
     </View>
