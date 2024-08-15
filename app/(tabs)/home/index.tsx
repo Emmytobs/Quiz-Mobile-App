47a17aeed8ac234e2f7b65b281fb2e37f7fc0dd5
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2024-08-16 00:01:25 +0100

    feature: implemented the Dashboard UI

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,28 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { FlatList, Image, Pressable, ScrollView, View } from "react-native";
 import { Text } from "~/components/ui/text";
 import * as AvatarPrimitive from "@rn-primitives/avatar";
 import { Bell, Filter, SearchIcon } from "lucide-react-native";
 import { useSession } from "~/stores/session";
-import type { User } from "~/stores/session";
 import { useColorScheme } from "~/lib/useColorScheme";
 import { Input } from "~/components/ui/input";
+import { useTranslation } from "react-i18next";
+import * as Progress from "react-native-progress";
 
 export default function HomeScreen() {
-  const user = useSession((state) => state.session?.user);
-
   return (
-    <View className="flex-1 p-4 pt-10">
-      <Header user={user} />
+    <ScrollView className="p-4 pt-10" showsVerticalScrollIndicator={false}>
+      <Header />
       <Search />
-    </View>
+      <Activities />
+      <QuickActions />
+      <Categories />
+    </ScrollView>
   );
 }
 
 const AVATAR_URI = "https://github.com/mrzachnugent.png";
 
-function Header({ user }: { user: User | undefined }) {
+function Title({ title }: { title: string }) {
+  return (
+    <Text className="font-interMedium text-md font-medium tracking-wide leading-normal">
+      {title}
+    </Text>
+  );
+}
+
+function Header() {
   const { isDarkColorScheme } = useColorScheme();
+  const user = useSession((state) => state.session?.user);
 
   return (
     <View className="flex-row items-center">
@@ -57,7 +68,7 @@ function Search() {
   const { isDarkColorScheme } = useColorScheme();
 
   return (
-    <View className="flex flex-row gap-4">
+    <View className="flex flex-row gap-4 py-6">
       <View className="flex-1 relative">
         <View className="absolute bottom-5 left-5">
           <SearchIcon
@@ -68,7 +79,7 @@ function Search() {
         <Input placeholder="Search for anything" className="pl-14" />
       </View>
       <View
-        className={`isDarkColorScheme ? "bg-[#161616]" : "bg-[#f6f6f6]"} p-4 rounded-lg items-center justify-center`}
+        className={`${isDarkColorScheme ? "bg-[#161616]" : "bg-[#f6f6f6]"} w-2/12 rounded-lg items-center justify-center`}
       >
         <Filter
           color={isDarkColorScheme ? "#fff" : "#0F0F0F"}
@@ -76,6 +87,165 @@ function Search() {
           fill={isDarkColorScheme ? "#fff" : "#0F0F0F"}
         />
       </View>
+    </View>
+  );
+}
+
+function Activities() {
+  const { t } = useTranslation("dashboard", { keyPrefix: "DashboardScreens" });
+
+  return (
+    <View className="w-full pt-4">
+      <Title title={t("Activities")} />
+
+      <FlatList
+        contentContainerClassName="my-4 gap-x-4"
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={["Hello", "Hi", "Hiii"]}
+        renderItem={({ item }) => <Activity />}
+      />
+    </View>
+  );
+}
+
+function Activity() {
+  const { t } = useTranslation("dashboard", { keyPrefix: "DashboardScreens" });
+  const { isDarkColorScheme } = useColorScheme();
+
+  return (
+    <View className="w-72 bg-[#FDBE1D] rounded-lg p-4 pt-6">
+      <Text className="text-md leading-normal tracking-wide font-medium">
+        {t("Last Quiz")}
+      </Text>
+      <Text className="leading-normal tracking-wide">
+        Introduction to Biology
+      </Text>
+      <View className="mt-4">
+        <Text className="text-sm leading-normal tracking-wide font-medium mb-3">
+          {t("You scored")} 8/10
+        </Text>
+        <Progress.Bar
+          animated={false}
+          progress={0.7}
+          width={null}
+          height={4}
+          color={isDarkColorScheme ? "#fff" : "#161616"}
+          unfilledColor={isDarkColorScheme ? "#161616" : "#fff"}
+          borderWidth={0}
+        />
+      </View>
+    </View>
+  );
+}
+
+function QuickActions() {
+  const { t } = useTranslation("dashboard", { keyPrefix: "DashboardScreens" });
+
+  return (
+    <View className="my-6">
+      <Title title={t("Quick Actions")} />
+
+      <View className="flex-row items-center justify-center gap-2 min-w-full my-4">
+        <QuickAction
+          title={t("Start Quiz")}
+          bgColor="bg-[#FBCD27]"
+          onPress={() => {}}
+        />
+        <QuickAction
+          title={t("Study Materials")}
+          bgColor="bg-[#13D282]"
+          onPress={() => {}}
+        />
+        <QuickAction
+          title={t("Create Group")}
+          bgColor="bg-[#FCA110]"
+          onPress={() => {}}
+        />
+      </View>
+    </View>
+  );
+}
+
+function QuickAction({
+  title,
+  bgColor,
+  onPress,
+}: {
+  title: string;
+  bgColor: string;
+  onPress: () => void;
+}) {
+  const { isDarkColorScheme } = useColorScheme();
+
+  return (
+    <Pressable
+      className={`rounded-lg shadow-sm shadow-black/40 items-center justify-center px-4 h-16 ${bgColor} flex-1`}
+      onPress={onPress}
+    >
+      <Text
+        className={`${isDarkColorScheme ? "#fff" : "#161616"} text-center leading-normal tracking-wide text-sm font-bold`}
+      >
+        {title}
+      </Text>
+    </Pressable>
+  );
+}
+
+const CategoriesData = [
+  {
+    image:
+      "https://images.pexels.com/photos/256262/pexels-photo-256262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Sciences",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1209843/pexels-photo-1209843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Arts",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/73910/mars-mars-rover-space-travel-robot-73910.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Technology",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/3729557/pexels-photo-3729557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Mathematics",
+  },
+];
+
+function Categories() {
+  const { t } = useTranslation("dashboard", { keyPrefix: "DashboardScreens" });
+  const { isDarkColorScheme } = useColorScheme();
+
+  return (
+    <View className="">
+      <Title title={t("Categories")} />
+
+      <FlatList
+        contentContainerClassName="flex-1 pt-5 pb-20"
+        scrollEnabled={false}
+        showsVerticalScrollIndicator={false}
+        data={CategoriesData}
+        numColumns={2}
+        ItemSeparatorComponent={() => (
+          <View className="w-4" style={{ height: 16 }} />
+        )}
+        renderItem={({ item, index }) => (
+          <View
+            className={`max-h-48 flex-1 w-full items-center ${isDarkColorScheme ? "bg-[#161616]" : "bg-[#f6f6f6]"} rounded-lg shadow-md shadow-black/20 overflow-hidden`}
+            style={{ marginRight: index % 2 === 0 ? 10 : 0 }}
+          >
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: "100%", height: "80%" }}
+              resizeMode="cover"
+            />
+            <Text className="leading-normal tracking-wide">{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,20 @@
-import '~/global.css';
+import "~/global.css";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Theme, ThemeProvider } from '@react-navigation/native';
-import { SplashScreen, Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { NAV_THEME } from '~/lib/constants';
-import { useColorScheme } from '~/lib/useColorScheme';
-import { PortalHost } from '@rn-primitives/portal';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useFonts } from 'expo-font';
-import { getCurrentLocale, useLocale } from '~/stores/locale';
-import i18n from '~/assets/translations/i18n';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { StateStorage } from '~/lib/stateStorage';
-import { Session, useSession } from '~/stores/session';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Theme, ThemeProvider } from "@react-navigation/native";
+import { SplashScreen, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
+import { PortalHost } from "@rn-primitives/portal";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { getCurrentLocale, useLocale } from "~/stores/locale";
+import i18n from "~/assets/translations/i18n";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StateStorage } from "~/lib/stateStorage";
+import { Session, useSession } from "~/stores/session";
 
 const LIGHT_THEME: Theme = {
   dark: false,
@@ -28,35 +28,35 @@ const DARK_THEME: Theme = {
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before getting the color scheme.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
-  const { colorScheme, isDarkColorScheme, setColorScheme } = useColorScheme()
-  const { locale, setLocale } = useLocale((state) => state)
-  const setSession = useSession(({ setSession }) => setSession)
+  const { colorScheme, isDarkColorScheme, setColorScheme } = useColorScheme();
+  const { locale, setLocale } = useLocale((state) => state);
+  const setSession = useSession(({ setSession }) => setSession);
 
   const [fontLoaded] = useFonts({
-    Satoshi: require('../assets/fonts/Satoshi-Medium.otf'),
+    Satoshi: require("../assets/fonts/Satoshi-Medium.otf"),
+    InterMedium: require("../assets/fonts/Inter-Medium.ttf"),
   });
 
   async function loadColorScheme() {
     try {
-      const theme = await AsyncStorage.getItem('theme');
+      const theme = await AsyncStorage.getItem("theme");
       if (!theme) {
         // TODO: Save the theme when there's a color mode switcher
         // await AsyncStorage.setItem('theme', colorScheme);
         return;
       }
       if (theme !== colorScheme) {
-        setColorScheme(theme as 'dark' | 'light');
+        setColorScheme(theme as "dark" | "light");
         return;
       }
-    }
-    finally {
+    } finally {
       setIsColorSchemeLoaded(true);
     }
   }
@@ -66,22 +66,22 @@ export default function RootLayout() {
     if (currentLocale !== i18n.language) {
       i18n.changeLanguage(currentLocale);
     }
-    setLocale(currentLocale)
+    setLocale(currentLocale);
   }
 
   async function loadSession() {
-    const session = await StateStorage.getItem<Session>('session')
+    const session = await StateStorage.getItem<Session>("session");
     if (session) {
       setSession(session);
     }
   }
-  
+
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-    React.useEffect(() => {
+  React.useEffect(() => {
     loadColorScheme();
     loadLocale();
     loadSession();
-  }, [])
+  }, []);
 
   React.useEffect(() => {
     if (fontLoaded && isColorSchemeLoaded && locale) {
@@ -89,7 +89,7 @@ export default function RootLayout() {
     }
   }, [locale, fontLoaded, isColorSchemeLoaded]);
 
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   if (!fontLoaded && !locale) {
     return null;
@@ -99,20 +99,14 @@ export default function RootLayout() {
     <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
       <SafeAreaProvider>
         <QueryClientProvider client={queryClient}>
-          <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+          <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
           <Stack>
             <Stack.Screen
               name="(onboarding)"
               options={{ headerShown: false }}
             />
-            <Stack.Screen
-              name="index"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="(tabs)"
-              options={{ headerShown: false }}
-            />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           </Stack>
           <PortalHost />
         </QueryClientProvider>
